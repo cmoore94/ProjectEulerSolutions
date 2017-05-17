094b17b682e32200311aacf8dfa804a9490e373e
Colm Moore <colmmoore1994@gmail.com>
2017-05-17 20:47:52 +0100

    Problem05 added.

---
+++ Problem05SmallestMultiple.py
@@ -0,0 +1,34 @@
+'''
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10
+without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+'''
+
+#Doesn't include 11 for optimization
+PRIMES_BETWEEN_TEN_AND_TWENTY = [11, 13, 17, 19]
+
+def find_smallest_multiple():
+    '''Returns smallest multiple of all number 1-20'''
+    i = 1
+    multiple = 0
+    while True:
+        #will have to be a multiple of 2520 for it to be divisible by 1-10
+        multiple = 2520 * i * 11    #multiply by 11 here to reduce run time
+        #If divisible, print number and exit
+        if check_if_divisible(multiple):
+            print multiple
+            exit()
+        i += 1
+
+def check_if_divisible(number):
+    '''checks if number is divisible and returns boolean'''
+    #checks if divisble by all primes
+    for j in PRIMES_BETWEEN_TEN_AND_TWENTY:
+
+        #if any primes don't go in evenly, return false.
+        if number % j != 0:
+            return False
+    return True
+
+find_smallest_multiple()
