d686e03a7468bb82f15744b184c431510d6ac2cc
Colm Moore <colmmoore1994@gmail.com>
2017-05-17 14:42:38 +0100

    Forgot to add comments

--- Problem1-MultiplesOf3And5.py
+++ Problem1-MultiplesOf3And5.py
@@ -1,9 +1,20 @@
+''' 
+Problem description:
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we 
+get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+'''
+
+
+#Sum set up at beginning to keep track.
 sum = 0
 
+#For loop between 0 and 1000.
 for i in range (0,1000):
-    if i % 3 == 0:
+    if i % 3 == 0:          #If divisible by 3
         sum += i
-    elif i % 5 == 0:
+    elif i % 5 == 0:        #else if divisible by 5
         sum += i
 
-print sum
+print sum                   #print answer
