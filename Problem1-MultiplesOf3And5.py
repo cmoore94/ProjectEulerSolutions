56faaec7de2880cbef2b873e3616f01e648e74d3
Colm Moore <colmmoore1994@gmail.com>
2017-05-17 14:56:27 +0100

    code style changes according to pyling, may as well do this right.

--- Problem1-MultiplesOf3And5.py
+++ Problem1-MultiplesOf3And5.py
@@ -1,20 +1,22 @@
-''' 
-Problem description:
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we 
+'''
+Problem 1.
+
+Description:
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we
 get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
 
-
 #Sum set up at beginning to keep track.
-sum = 0
+sumOfMultiples = 0
 
 #For loop between 0 and 1000.
-for i in range (0,1000):
+for i in range(0, 1000):
     if i % 3 == 0:          #If divisible by 3
-        sum += i
+        sumOfMultiples += i
     elif i % 5 == 0:        #else if divisible by 5
-        sum += i
+        sumOfMultiples += i
 
-print sum                   #print answer
+print sum                   #print answerpip
